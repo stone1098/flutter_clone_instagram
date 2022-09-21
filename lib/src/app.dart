217864eaccafbfc34cb99ac080167238302b9b5a
Shin <stone1098@naver.com>
2022-09-21 20:09:56 +0900

    first commit

---
+++ lib/src/app.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clone_instagram/src/components/image_data.dart';
+import 'package:flutter_clone_instagram/src/controller/bottom_nav_controller.dart';
+import 'package:get/get.dart';
+
+class App extends GetView<BottomNavController> {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        child: Scaffold(
+          appBar: AppBar(),
+          body: Container(),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            currentIndex: controller.pageIndex.value,
+            onTap: (value){
+              controller.changeBottomNav,
+            },
+            elevation: 0,
+            items: [
+              BottomNavigationBarItem(
+                icon: ImageData(IconsPath.homeOff),
+                activeIcon: ImageData(IconsPath.homeOn),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: ImageData(IconsPath.searchOff),
+                activeIcon: ImageData(IconsPath.searchOn),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: ImageData(IconsPath.uploadIcon),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: ImageData(IconsPath.activeOff),
+                activeIcon: ImageData(IconsPath.activeOn),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey
+                  ),
+                  width: 30,
+                  height: 30,
+                ),
+                label: 'home',
+              ),
+            ],
+          ),
+        ),
+        onWillPop: () async{
+          return false;
+        //  갤럭시 뒤로가기 버튼
+        }
+    );
+  }
+}
